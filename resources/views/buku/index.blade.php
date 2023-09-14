25c627a546a4134bc250ddba162bd9f36efc8b41
aminhnh <109867158+aminhnh@users.noreply.github.com>
2023-09-14 15:25:10 +0700

    first commit

---
+++ resources/views/buku/index.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Books</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</head>
+<body>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Judul Buku</th>
+                <th>Penulis</th>
+                <th>Harga</th>
+                <th>Tanggal Terbit</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($data_buku as $buku)
+                <tr>
+                    <td>{{ $buku->id }}</td>
+                    <td>{{ $buku->judul }}</td>
+                    <td>{{ $buku->penulis }}</td>
+                    <td>{{ "Rp ".number_format($buku->harga, 2, ',', '.') }}</td>
+                    <td>{{ \Carbon\Carbon::parse($buku->tgl_terbit)->format('d/m/Y')}}</td>
+                    <td><button class="btn btn-primary" type="submit">Edit</button></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+</html>
